509e0b738371eacac905149b983fd30b00eff1d4
Victor Lapin <victor@slimroms.org>
2018-06-10 21:53:23 +0300

    fix obfuscated field names in exported file

--- src/com/victorlapin/flasher/model/database/entity/Command.kt
+++ src/com/victorlapin/flasher/model/database/entity/Command.kt
@@ -3,16 +3,21 @@ package com.victorlapin.flasher.model.database.entity
 import android.arch.persistence.room.ColumnInfo
 import android.arch.persistence.room.Entity
 import android.arch.persistence.room.PrimaryKey
+import com.google.gson.annotations.SerializedName
 
 @Entity(tableName = "commands")
 data class Command(
         @PrimaryKey(autoGenerate = true)
+        @SerializedName("id")
         val id: Long? = null,
         @ColumnInfo(name = "type")
+        @SerializedName("type")
         var type: Int,
         @ColumnInfo(name = "arg1")
+        @SerializedName("arg1")
         var arg1: String? = null,
         @ColumnInfo(name = "arg2")
+        @SerializedName("arg2")
         var arg2: String? = null
 ) {
     companion object {
