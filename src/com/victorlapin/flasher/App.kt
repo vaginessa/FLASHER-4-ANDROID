db800f085994d4023369ec4104302d08a61c6a4b
Victor Lapin <victor@slimroms.org>
2018-06-11 10:52:30 +0300

    disable koin logs in release builds

--- src/com/victorlapin/flasher/App.kt
+++ src/com/victorlapin/flasher/App.kt
@@ -3,10 +3,13 @@ package com.victorlapin.flasher
 import android.app.Application
 import com.victorlapin.flasher.di.allModules
 import org.koin.android.ext.android.startKoin
+import org.koin.android.logger.AndroidLogger
+import org.koin.log.EmptyLogger
 
 class App : Application() {
     override fun onCreate() {
         super.onCreate()
-        startKoin(this, allModules)
+        val logger = if (BuildConfig.DEBUG) AndroidLogger() else EmptyLogger()
+        startKoin(application = this, modules = allModules, logger = logger)
     }
 }
