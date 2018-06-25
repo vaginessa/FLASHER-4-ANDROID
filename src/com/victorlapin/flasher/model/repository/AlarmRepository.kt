a677fb24da3d4f058eebbe18b6cbb9bbbe2b5e70
Victor Lapin <victor@slimroms.org>
2018-06-25 12:42:38 +0300

    alarm repository: don't set alarm if time is not specified

--- src/com/victorlapin/flasher/model/repository/AlarmRepository.kt
+++ src/com/victorlapin/flasher/model/repository/AlarmRepository.kt
@@ -14,12 +14,14 @@ class AlarmRepository(
         val time = mSettings.scheduleTime
         val periodDays = mSettings.schedulePeriod
 
-        if (periodDays > 0) {
-            mServices.alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, time,
-                    TimeUnit.DAYS.toMillis(periodDays.toLong()), mServices.alarmIntent)
-        } else {
-            mServices.alarmManager.setWindow(AlarmManager.RTC_WAKEUP, time,
-                    TimeUnit.HOURS.toMillis(1), mServices.alarmIntent)
+        if (time > 0) {
+            if (periodDays > 0) {
+                mServices.alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, time,
+                        TimeUnit.DAYS.toMillis(periodDays.toLong()), mServices.alarmIntent)
+            } else {
+                mServices.alarmManager.setWindow(AlarmManager.RTC_WAKEUP, time,
+                        TimeUnit.HOURS.toMillis(1), mServices.alarmIntent)
+            }
         }
         emitter.onSuccess(Any())
     }
