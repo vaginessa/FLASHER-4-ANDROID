cc33546c2209e61a02a57b0fa32d590472d0d95b
Victor Lapin <victor@slimroms.org>
2018-06-25 14:13:10 +0300

    alarm repository: add log

--- src/com/victorlapin/flasher/model/repository/AlarmRepository.kt
+++ src/com/victorlapin/flasher/model/repository/AlarmRepository.kt
@@ -1,9 +1,13 @@
 package com.victorlapin.flasher.model.repository
 
 import android.app.AlarmManager
+import android.util.Log
 import com.victorlapin.flasher.manager.ServicesManager
 import com.victorlapin.flasher.manager.SettingsManager
 import io.reactivex.Single
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 import java.util.concurrent.TimeUnit
 
 class AlarmRepository(
@@ -15,6 +19,9 @@ class AlarmRepository(
         val periodDays = mSettings.schedulePeriod
 
         if (time > 0) {
+            Log.i("Alarm",
+                    "Next run: ${SimpleDateFormat.getDateTimeInstance(DateFormat.MEDIUM,
+                            DateFormat.SHORT).format(Date(time))}")
             if (periodDays > 0) {
                 mServices.alarmManager.setInexactRepeating(AlarmManager.RTC_WAKEUP, time,
                         TimeUnit.DAYS.toMillis(periodDays.toLong()), mServices.alarmIntent)
@@ -27,6 +34,7 @@ class AlarmRepository(
     }
 
     fun cancelAlarm(): Single<Any> = Single.create { emitter ->
+        Log.i("Alarm", "Canceled")
         mServices.alarmManager.cancel(mServices.alarmIntent)
         emitter.onSuccess(Any())
     }
