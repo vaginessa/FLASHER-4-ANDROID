d644ec9abab6aaa5fabaea8a7c4d7d0854e5d2c2
Victor Lapin <victor@slimroms.org>
2018-07-24 19:29:10 +0300

    Revert "boot receiver: remove schedule part"
    This reverts commit db5507410e0572ee613919a2ea1baf44f06c4dd1.

--- src/com/victorlapin/flasher/presenter/BootReceiverPresenter.kt
+++ src/com/victorlapin/flasher/presenter/BootReceiverPresenter.kt
@@ -2,11 +2,20 @@ package com.victorlapin.flasher.presenter
 
 import com.victorlapin.flasher.manager.ServicesManager
 import com.victorlapin.flasher.manager.SettingsManager
+import com.victorlapin.flasher.model.interactor.AlarmInteractor
+import timber.log.Timber
 
 class BootReceiverPresenter(
         private val mSettings: SettingsManager,
-        private val mServices: ServicesManager
+        private val mServices: ServicesManager,
+        private val mAlarmInteractor: AlarmInteractor
 ) {
+    fun resetAlarm() {
+        mAlarmInteractor.setAlarm()
+                .doOnError { Timber.e(it) }
+                .subscribe()
+    }
+
     fun showNotification() {
         if (mSettings.showNotificationOnBoot && mSettings.bootNotificationFlag) {
             mServices.showBootNotification(mSettings.scheduleLastRun)
--- src/com/victorlapin/flasher/ui/receivers/BootReceiver.kt
+++ src/com/victorlapin/flasher/ui/receivers/BootReceiver.kt
@@ -6,12 +6,15 @@ import android.content.Intent
 import com.victorlapin.flasher.presenter.BootReceiverPresenter
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
+import timber.log.Timber
 
 class BootReceiver : BroadcastReceiver(), KoinComponent {
     private val mPresenter by inject<BootReceiverPresenter>()
 
     override fun onReceive(context: Context, intent: Intent) {
         if (intent.action == "android.intent.action.BOOT_COMPLETED") {
+            Timber.i("Recreating scheduled job on boot")
+            mPresenter.resetAlarm()
             mPresenter.showNotification()
         }
     }
