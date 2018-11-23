3716b785cdc2a45c029107f770ee47c0f7b90ac3
Victor Lapin <victor@slimroms.org>
2018-11-23 14:35:01 +0300

    backup repository: add extensive logging to locate user's issue

--- src/com/victorlapin/flasher/model/repository/BackupsRepository.kt
+++ src/com/victorlapin/flasher/model/repository/BackupsRepository.kt
@@ -7,6 +7,7 @@ import java.util.*
 
 class BackupsRepository {
     fun deleteObsoleteBackups(backupsToKeep: Int) {
+        Timber.i("Going to delete old backups, will keep $backupsToKeep")
         val twrpFolder = File(Const.TWRP_FOLDER)
         if (twrpFolder.exists()) {
             val backupsFolder = File(twrpFolder, "BACKUPS")
@@ -16,24 +17,36 @@ class BackupsRepository {
                     val backups = profiles[0].listFiles { file, name ->
                         name.endsWith("_Flasher") && File(file, name).isDirectory
                     }
-                    if (backups.isNotEmpty() && backups.size > backupsToKeep - 1) {
-                        Arrays.sort(backups) { f1, f2 ->
-                            val dateDiff = f2.lastModified() - f1.lastModified()
-                            when {
-                                (dateDiff < 0) -> -1
-                                (dateDiff > 0) -> 1
-                                else -> 0
+                    if (backups.isNotEmpty()) {
+                        if (backups.size > backupsToKeep - 1) {
+                            Arrays.sort(backups) { f1, f2 ->
+                                val dateDiff = f2.lastModified() - f1.lastModified()
+                                when {
+                                    (dateDiff < 0) -> -1
+                                    (dateDiff > 0) -> 1
+                                    else -> 0
+                                }
                             }
+                            val backupsToDelete = backups.drop(backupsToKeep - 1)
+                            backupsToDelete.forEach {
+                                Timber.i("Deleting backup: ${it.name}")
+                                it.deleteRecursively()
+                                Timber.i("Successful")
+                            }
+                        } else {
+                            Timber.i("Found ${backups.size} backups, no need to delete anything")
                         }
-                        val backupsToDelete = backups.drop(backupsToKeep - 1)
-                        backupsToDelete.forEach {
-                            Timber.i("Deleting backup: ${it.name}")
-                            it.deleteRecursively()
-                            Timber.i("Successful")
-                        }
+                    } else {
+                        Timber.w("No backups created by Flasher found")
                     }
+                } else {
+                    Timber.w("Backup profiles can't be found (file listing is null or empty)")
                 }
+            } else {
+                Timber.w("Folder '${backupsFolder.absolutePath}' can't be found")
             }
+        } else {
+            Timber.w("Folder '${twrpFolder.absolutePath}' can't be found")
         }
     }
 }
