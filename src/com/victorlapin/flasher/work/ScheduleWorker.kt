a89d4e02ff7807b100de2cb10bf4975d5205a571
Victor Lapin <victor@slimroms.org>
2018-12-10 18:37:28 +0300

    work manager: implement reactive worker

--- src/com/victorlapin/flasher/work/ScheduleWorker.kt
+++ src/com/victorlapin/flasher/work/ScheduleWorker.kt
@@ -1,56 +1,52 @@
 package com.victorlapin.flasher.work
 
 import android.content.Context
-import androidx.work.Constraints
-import androidx.work.OneTimeWorkRequest
-import androidx.work.Worker
-import androidx.work.WorkerParameters
+import androidx.work.*
 import com.victorlapin.flasher.Const
 import com.victorlapin.flasher.manager.ServicesManager
 import com.victorlapin.flasher.manager.SettingsManager
 import com.victorlapin.flasher.model.database.entity.Chain
 import com.victorlapin.flasher.model.interactor.RecoveryScriptInteractor
+import io.reactivex.Completable
+import io.reactivex.Single
 import org.koin.standalone.KoinComponent
 import org.koin.standalone.inject
 import timber.log.Timber
 import java.util.concurrent.TimeUnit
 
 class ScheduleWorker(context: Context, params: WorkerParameters) :
-        Worker(context, params), KoinComponent {
+        RxWorker(context, params), KoinComponent {
     private val mSettings by inject<SettingsManager>()
     private val mScriptInteractor by inject<RecoveryScriptInteractor>()
     private val mServices by inject<ServicesManager>()
 
-    override fun doWork(): Result {
-        val scope = getKoin().createScope(Const.WORKER_SCHEDULE)
-        Timber.i("Schedule worker started")
-        mSettings.scheduleLastRun = System.currentTimeMillis()
-        return try {
-            val scriptResult = mScriptInteractor
-                    .buildScript(Chain.SCHEDULE_ID)
-                    .blockingGet()
-            val result = mScriptInteractor.deployScript(scriptResult.script)
-            if (result.isSuccess) {
-                mSettings.bootNotificationFlag = true
-                mScriptInteractor.rebootRecovery().subscribe()
-            } else {
-                mSettings.useSchedule = false
-                mServices.showInfoNotification(result)
+    override fun createWork(): Single<Result> = mScriptInteractor
+            .buildScript(Chain.SCHEDULE_ID)
+            .map { mScriptInteractor.deployScript(it.script) }
+            .flatMapCompletable { args ->
+                if (args.isSuccess) {
+                    mSettings.bootNotificationFlag = true
+                    mScriptInteractor.rebootRecovery().subscribe()
+                } else {
+                    mSettings.useSchedule = false
+                    mServices.showInfoNotification(args)
+                }
+                Completable.complete()
             }
-            Timber.i("Schedule worker finished")
-            Result.SUCCESS
-        } catch (t: Throwable) {
-            Timber.e(t)
-            mServices.showInfoNotification(t.message)
-            Result.RETRY
-        } finally {
-            scope.close()
-        }
-    }
-
-    override fun onStopped() {
-        Timber.w("Schedule worker stopped")
-    }
+            .toSingleDefault(androidx.work.Result.success())
+            .onErrorReturnItem(androidx.work.Result.retry())
+            .doOnSubscribe {
+                Timber.i("Schedule worker started")
+                getKoin().createScope(Const.WORKER_SCHEDULE)
+            }
+            .doOnError {
+                Timber.e(it)
+                mServices.showInfoNotification(it.message)
+            }
+            .doFinally {
+                Timber.i("Schedule worker finished")
+                getKoin().getScope(Const.WORKER_SCHEDULE).close()
+            }
 
     companion object {
         const val JOB_TAG = "ScheduleWorker"
